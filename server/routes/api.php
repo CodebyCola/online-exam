5938ff3d40e1d5f45463ff7be706accf577943c1
Nico <nicolausnl05@gmail.com>
2026-09-20 13:52:23 +0700

    add auth api route

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+require __DIR__.'/api/auth.php';
+require __DIR__.'/api/exams.php';
+require __DIR__.'/api/student.php';
+require __DIR__.'/api/results.php';
